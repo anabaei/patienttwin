00ff60c89520491cb1deb5b851dba548d7471752
Anuj Wagle <anuj@twinnlinks.com>
2025-09-12 07:37:53 -0400

    trigger build

--- patient-portal/next.config.mjs
+++ patient-portal/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Image optimization
   images: {
     domains: ['localhost', 'images.unsplash.com', 'plus.unsplash.com', 'source.unsplash.com', 'picsum.photos', 'i.pravatar.cc', '1000logos.net', 'cdn.worldvectorlogo.com', 'dt8n8gomznf9q.cloudfront.net', 'encrypted-tbn0.gstatic.com'],
   },
