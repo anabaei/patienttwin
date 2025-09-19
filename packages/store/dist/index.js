e901042aee1d4846dcb39c6a2a865456d24173fb
Anuj Wagle <anuj@twinn.ca>
2025-09-19 19:54:26 -0400

    include build instructions

--- packages/store/dist/index.js
+++ packages/store/dist/index.js
@@ -4,12 +4,12 @@ export * from './types';
 export { useAppointmentsStore } from './stores/appointments';
 export { useAuthStore } from './stores/auth';
 export { useAvailabilityStore } from './stores/availability';
+export { useChatStore } from './stores/chat';
 export { useDirectoryStore } from './stores/directory';
 export { useHealthcareBalancesStore } from './stores/healthcare-balances';
 export { useInsuranceStore } from './stores/insurance';
 export { useNavigationStore } from './stores/navigation';
 export { useSettingsStore } from './stores/settings';
-export { useChatStore } from './stores/chat';
 // Utilities
 export * from './utils/availability';
 export * from './utils/geo';
